9dd9945438882a8793ba7d09e222d5d065ecdb74
Chris Marrin <chris@marrin.com>
2022-03-01 16:34:31 -0800

    Initial checkin of Test files

---
+++ Test/Test.ino
@@ -0,0 +1,10 @@
+/*-------------------------------------------------------------------------
+    This source file is a part of Clover
+    For the latest info, see https://github.com/cmarrin/Clover
+    Copyright (c) 2021-2022, Chris Marrin
+    All rights reserved.
+    Use of this source code is governed by the MIT license that can be
+    found in the LICENSE file.
+-------------------------------------------------------------------------*/
+
+
